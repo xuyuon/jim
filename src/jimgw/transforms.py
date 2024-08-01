2b29c48019df199066f1e5f2fa7a60ea380ce872
xuyuon <116078673+xuyuon@users.noreply.github.com>
2024-08-01 16:15:32 -0400

    Updated transforms.py

--- src/jimgw/transforms.py
+++ src/jimgw/transforms.py
@@ -7,6 +7,8 @@ from chex import assert_rank
 from beartype import beartype as typechecker
 from jaxtyping import Float, Array, jaxtyped
 
+from jimgw.single_event.utils import spin_to_cartesian_spin
+
 
 class Transform(ABC):
     """
@@ -443,3 +445,40 @@ class ParetoTransform(BijectiveTransform):
             )
             for i in range(len(name_mapping[1]))
         }
+
+
+class SpinToCartesianSpinTransform(NtoMTransform):
+    """
+    Spin to Cartesian spin transformation
+    """
+
+    freq_ref: Float
+    phase_c: Float
+    
+    def __init__(
+        self,
+        name_mapping: tuple[list[str], list[str]],
+        freq_ref: Float,
+        phase_c: Float,
+    ):
+        super().__init__(name_mapping)
+        
+        self.freq_ref = freq_ref
+        self.phase_c = phase_c
+        
+        def named_transform(x):
+            theta_jn = x[name_mapping[0][0]]
+            phi_jl = x[name_mapping[0][1]]
+            theta1 = x[name_mapping[0][2]]
+            theta2 = x[name_mapping[0][3]]
+            phi12 = x[name_mapping[0][4]]
+            a1 = x[name_mapping[0][5]]
+            a2 = x[name_mapping[0][6]]
+            M_c = x[name_mapping[0][7]]
+            q = x[name_mapping[0][8]]
+            iota, s1x, s1y, s1z, s2x, s2y, s2z = spin_to_cartesian_spin(
+                theta_jn, phi_jl, theta1, theta2, phi12, a1, a2, M_c, q, self.freq_ref, self.phase_c
+            )
+            return {name_mapping[1][0]: iota, name_mapping[1][1]: s1x, name_mapping[1][2]: s1y, name_mapping[1][3]: s1z, name_mapping[1][4]: s2x, name_mapping[1][5]: s2y, name_mapping[1][6]: s2z}
+        
+        self.transform_func = named_transform
