c8f0acd31c8f52d7ee7219bd0e7887051b3e6dc0
xuyuon <116078673+xuyuon@users.noreply.github.com>
2024-08-30 00:06:13 +0800

    Fix

--- src/jimgw/single_event/runManager.py
+++ src/jimgw/single_event/runManager.py
@@ -461,6 +461,7 @@ class SingleEventPERunManager(RunManager):
     def save_summary(self, path: str = "", **kwargs):
         if path == "":
             path = self.run.path + "run_manager_summary.txt"
+        orig_stdout = sys.stdout
         sys.stdout = open(path, "wt")
         self.jim.print_summary()
         if self.run.injection:
@@ -469,6 +470,7 @@ class SingleEventPERunManager(RunManager):
             networkSNR = jnp.sum(jnp.array(self.SNRs) ** 2) ** (0.5)
             print("network SNR is", networkSNR)
         sys.stdout.close()
+        sys.stdout=orig_stdout 
 
 
 class MultipleEventRunManager:
